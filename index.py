089522cdc368203766ba9b84133858b446fb3d47
ankushKun <ankush4singh@gmail.com>
2021-12-25 16:09:48 +0530

    fuck vercel

---
+++ index.py
@@ -0,0 +1,2 @@
+from os import system
+system("cd src && python3 app.py")
--- src/app.py
+++ src/app.py
@@ -51,4 +51,4 @@ def submit():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3305, debug=False)
+    app.run(debug=False)
